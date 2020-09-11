80b75814e3b91ffe43fe543de702a91bf4ce1145
Lucas van Diepen <lucascool@ziggo.nl>
2020-09-11 14:28:07 +0200

    Added features

--- Beroepsopdracht/product 1.py
+++ Beroepsopdracht/product 1.py
@@ -1,5 +1,17 @@
-print("Hello You!, Ik ben Lucas")
-print("Wie ben jij?")
-name = input()
-print("Hello " + name)
-input()
+import datetime
+
+while True:
+    print("Hello You!, Ik ben Lucas")
+    print("Wie ben jij?")
+    name = input()
+    print("Hello " + name)
+    print("De datum en tijd is " + str(datetime.datetime.now()))
+    while True:
+        againInput = input(name + " wil jij dit programma nog een keer doen? Type Y/N: ")
+        if againInput.upper() == "Y":
+            break
+        elif againInput.upper() == "N":
+            print("Oke. Dankjewel")
+            exit()
+        else:
+            print("\nKies tussen Y of N")
