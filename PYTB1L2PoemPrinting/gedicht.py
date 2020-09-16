32ade2220425145831fda1d1e2a4c7c523026973
Lucas van Diepen <lucascool@ziggo.nl>
2020-09-16 14:53:36 +0200

    Gedicht bijgewerkt

--- PYTB1L2PoemPrinting/gedicht.py
+++ PYTB1L2PoemPrinting/gedicht.py
@@ -1,4 +1,8 @@
-vers1 = 'vers 1'
-vers2 = 'vers 2'
+vers1 = "Ik ben Lucas van Diepen"
+vers2 = "en ik houd van typen."
+vers3 = "Ook houd ik van games."
+vers4 = "Die draai ik op veel frames."
+vers5 = "Call of duty is mijn favoriete game."
+vers6 = "Alleen winnen is een probleem."
 
-print(vers1, vers2)
+print(vers1, vers2, vers3, vers4, vers5, vers6)
