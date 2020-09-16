68fd30e7b51c696211b61b6f33fc9e130a120bd8
Lucas van Diepen <lucascool@ziggo.nl>
2020-09-16 15:33:13 +0200

    Added playerstats.py

---
+++ PYTB1L2PlayerStatistics/playerstats.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+class Gun(Enum):
+    def __str__(self):
+        return str(self.value)
+    
+    Pistol = "Pistol"
+    SMG = "Sub Machine Gun"
+    AR = "Assult Rifle"
+    Sniper = "Sniper"
+
+def newPlayer(name, age, strength, gun, ammo, alive, level, kills, lives, money):
+    newPlayer = {
+        "Name": name,
+        "Age": age,
+        "Strength": strength,
+        "Gun": gun,
+        "Ammo": ammo,
+        "Alive": alive,
+        "Level": level,
+        "Kills": kills,
+        "Lives": lives,
+        "Money": money
+    }
+    return newPlayer
+
+player = newPlayer("Lucas", 16, 100, Gun.Sniper, 40, True, 99, 18, 3, 66.55)
+
+for key in player:
+    value = player[key]
+    print(key + ": " + str(value))
