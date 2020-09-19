1afcf3a5ce86924be355e6d353296221b840e01d
Lucas van Diepen <lucascool@ziggo.nl>
2020-09-19 18:48:55 +0200

    Simplified running commands

--- Beroepsopdracht/textbasedapp.py
+++ Beroepsopdracht/textbasedapp.py
@@ -1,6 +1,7 @@
 class Item:
-    def __init__(self, name, tags):
+    def __init__(self, name, pickable, tags):
         self.name = name
+        self.pickable = pickable
         self.tags = tags
 
 class Room:
@@ -10,12 +11,18 @@ class Room:
         self.items = items
         self.roomDirections = roomDirections #make this a dictionary
 
+class Command:
+    def __init__(self, function, args, expectedArgs):
+        self.function = function
+        self.args = args
+        self.expectedArgs = expectedArgs
+
 rooms = []
 commands = {
-    "n,north": "GoDirection(n)",
-    "w,west": "GoDirection(w)",
-    "e,east": "GoDirection(e)",
-    "s,south": "GoDirection(s)"
+    "n,north": Command("GoDirection", "n", 0),
+    "w,west": Command("GoDirection", "w", 0),
+    "e,east": Command("GoDirection", "e", 0),
+    "s,south": Command("GoDirection", "s", 0)
 }
 
 def FindFunction(s):
@@ -30,8 +37,9 @@ def FindArgs(s):
 def GoDirection(args):
     print(args[0])
 
-def RunCommand(func):
-    globals()[FindFunction(func)](FindArgs(func))
+def RunCommand(command):
+    #globals()[FindFunction(func)](FindArgs(func))
+    globals()[command.function](command.args)
 
 
 #RunCommand("GoDirection(teststring,moreteststrings,plusthis)")
@@ -46,13 +54,36 @@ def GetCommand(playerCommand):
 
 def AskCommand():
     playerInput = input("> ")
-    inputInList = playerInput.split(" ")
-    
-    commandFunction = GetCommand(inputInList[0])
-    if(commandFunction is None or commandFunction == ""):
-        print("Command not found")
+    if(len(playerInput) <= 0):
+        print("No input")
     else:
-        RunCommand(commandFunction)
+        inputInList = playerInput.split(" ")
+        command = GetCommand(inputInList[0])
+        if(command is None):
+            print("Command not found")
+        else:
+            argsCount = len(inputInList) - 1
+            if(command.expectedArgs == 0):
+                if(argsCount > 0):
+                    print("Don't know what you mean")
+                else:
+                    RunCommand(command)
+            elif(command.expectedArgs == -1):
+                    pass
+                    #no limit for arguments, so send all arguments
+                    RunCommand(Command(command.function, inputInList[1:len(inputInList)], command.expectedArgs))
+            else:
+                if(command.expectedArgs == argsCount):
+                    RunCommand(Command(command.function, inputInList[1:len(inputInList)], command.expectedArgs))
+                elif(argsCount < command.expectedArgs and argsCount >= 0):
+                    print("Don't know what you mean yet. Follow up question should happen here")
+                elif(argsCount > command.expectedArgs):
+                    print("Don't know what you mean")
+                
+                    
+    
+        
+        
 
 #Setup process
 
