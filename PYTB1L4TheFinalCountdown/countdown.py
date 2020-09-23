32b0ed7602647bae55fdad6f918ef7dee5c9aade
Lucas van Diepen <lucascool@ziggo.nl>
2020-09-23 15:06:40 +0200

    Added countdown.py

---
+++ PYTB1L4TheFinalCountdown/countdown.py
@@ -0,0 +1,17 @@
+import time
+
+soundCantPlay = False
+
+try:
+    from playsound import playsound
+except ModuleNotFoundError:
+    soundCantPlay = True
+
+for i in range(1000, 0, -1):
+    print(i)
+    time.sleep(0.01)
+
+if(soundCantPlay):
+    print("BOEM")
+else:
+    playsound("C:\\Users\\Lucas\\Desktop\\School\\PythonAchievements\\PYTB1L4TheFinalCountdown\\boom.mp3")
