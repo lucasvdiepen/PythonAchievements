ffdbd53394898122a34b575e6b7a13e6d872a861
Lucas van Diepen <lucascool@ziggo.nl>
2020-09-24 21:31:38 +0200

    Updated ditbenik.py voor keuzeverhaal

--- Beroepsopdracht/ditbenik.py
+++ Beroepsopdracht/ditbenik.py
@@ -11,6 +11,21 @@ chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "
 correctResponses = ["Goed!", "Klopt!", "Correct!", "Goed gegokt"]
 incorrectResponses = ["Fout", "Helaas", "Verkeerd gegokt"]
 
+class Question():
+    def __init__(self, name, question, options):
+        self.name = name
+        self.question = question
+        self.options = options
+
+questions = []
+
+#questions.append(Question("", "", {"": "", "": "", "": ""}))
+questions.append(Question("Woon", "Waar woon ik?", {"Alkmaar": "Leeftijd", "Amsterdam": "Leeftijd", "Heerhugowaard": "Leeftijd"}))
+questions.append(Question("Leeftijd", "Hoe oud ben ik?", {"17": "Niveau", "15": "Niveau", "16": "Niveau"}))
+questions.append(Question("Niveau", "Welk niveau heb ik vorig jaar gedaan", {"havo": "Snack", "vwo": "Snack", "vmbo-t": "Snack"}))
+questions.append(Question("Snack", "Wat is mijn lievelings snack?", {"Frikandel": "", "Kaassouflé": "", "Kroket": "Groente"}))
+questions.append(Question("Groente", "Oke dat klopt. \n\nWat is dan mijn lievelings groente?", {"Broccoli": "", "Wortels": "", "Bloemkool": ""}))
+
 def ShowTextAnimation(text):
     for char in text:
         sys.stdout.write(char)
@@ -18,17 +33,38 @@ def ShowTextAnimation(text):
         time.sleep(dialogCharDelay)
     print("")
 
+def GetQuestion(questionName):
+    for question in questions:
+        if(question.name == questionName):
+            return question
+
+def AskQuestions():
+    nextQuestion = "Woon"
+    while True:
+        currentQuestion = GetQuestion(nextQuestion)
+        nextQuestion = AskQuestion(currentQuestion.question, currentQuestion.options)
+        if(nextQuestion == "" or nextQuestion is None):
+            break
+
+    print("Klaar.")
+
+
 def AskQuestion(question, options):
     if len(options) > len(chars):
         return
     rndChoices = list(range(0, len(options)))
     correctAnswerIndex = -1
 
+    nextQuestions = {"nothing": "nothing"}
+
     print(question + "\n")
 
     for j in range(len(rndChoices)):
         rnd = random.choice(rndChoices)
-        print(chars[j] + ". " + options[rnd])
+        values = options.values()
+        nextQuestions[chars[j]] = list(values)[rnd]
+        keys = options.keys()
+        print(chars[j] + ". " + list(keys)[rnd])
         if(rnd == (len(options) - 1)):
             correctAnswerIndex = j
         
@@ -46,12 +82,13 @@ def AskQuestion(question, options):
                 
                 time.sleep(1)
 
-                break
+                print("\n")
+                return nextQuestions[answer.upper()]
 
             else: print("Kies de letter van een van de antwoorden \n\n")
         else: print("Kies de letter van een van de antwoorden \n\n")
     
-    print("\n")
+    
 
 
 while True:
@@ -62,9 +99,10 @@ while True:
     ShowTextAnimation("Hello " + name)
     print("De datum en tijd is " + str(datetime.datetime.now()) + "\n\n")
     #Het laatste antwoord moet altijd het goede antwoord zijn
-    AskQuestion("Waar woon ik?", ["Alkmaar", "Amsterdam", "Heerhugowaard"])
-    AskQuestion("Welk niveau heb ik vorig jaar gedaan", ["havo", "vwo", "vmbo-t"])
-    AskQuestion("Hoe oud ben ik?", ["17", "15", "16"])
+    #AskQuestion("Waar woon ik?", ["Alkmaar", "Amsterdam", "Heerhugowaard"])
+    #AskQuestion("Welk niveau heb ik vorig jaar gedaan", ["havo", "vwo", "vmbo-t"])
+    #AskQuestion("Hoe oud ben ik?", ["17", "15", "16"])
+    AskQuestions()
     while True:
         againInput = input(name + " wil jij dit programma nog een keer doen? Type Y/N: ")
         if againInput.upper() == "Y":
