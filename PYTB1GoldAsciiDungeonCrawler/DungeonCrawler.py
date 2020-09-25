4b391a7d5e237ba02338e3ebe050cbb57cfb333a
Lucas van Diepen <lucascool@ziggo.nl>
2020-09-25 15:15:00 +0200

    Uodated DungeonCrawler.py

--- PYTB1GoldAsciiDungeonCrawler/DungeonCrawler.py
+++ PYTB1GoldAsciiDungeonCrawler/DungeonCrawler.py
@@ -3,12 +3,42 @@ import sys
 import time
 from pynput.keyboard import Key, Listener
 import queue
+import random
 clear = lambda: os.system('cls')
 
-AttackDelay = 1500 # in milliseconds
-EnemyAttackDelay = 2500
+asciiArt = """                   (    )
+                  ((((()))
+                  |o\ /o)|
+                  ( (  _')
+                   (._.  /\__
+                  ,\___,/ '  ')
+    '.,_,,       (  .- .   .    )
+     \   \\     ( '        )(    )
+      \   \\    \.  _.__ ____( .  |
+       \  /\\   .(   .'  /\  '.  )
+        \(  \\.-' ( /    \/    \)
+         '  ()) _'.-|/\/\/\/\/\|
+             '\\ .( |\/\/\/\/\/|
+               '((  \    /\    /
+               ((((  '.__\/__.')
+                ((,) /   ((()   )
+                 "..-,  (()("   /
+            pils  _//.   ((() ."
+          _____ //,/" ___ ((( ', ___
+                           ((  )
+                            / /
+                          _/,/'
+                        /,/,"
+"""
+
+AttackDelay = 1000 # in milliseconds
+EnemyAttackDelay = 2000 # in milliseconds
 
 callback_queue = queue.Queue()
+
+userInput = ""
+
+MoveKeyDown = None
 
 class bcolors:
     HEADER = '\033[95m'
@@ -26,9 +56,12 @@ class Position():
         self.y = y
 
 class Player():
-    health = 5
+    health = 20
     position = Position(2, 5)
     canMove = True
+    cantMoveStartTime = 0
+    minDamage = 2
+    maxDamage = 8
 
     def Damage(self, damage):
         self.health -= damage
@@ -44,26 +77,40 @@ class Player():
             pass
         elif(charOnNewPosition == "x"):
             #enemy
-            enemy = game.GetEnemy(newPosition)
+            enemy = game.GetEnemy(newPosition)#crashes sometimes because enemy move away
             enemy.canMove = False
             self.canMove = False
-            enemy.Damage(2)
+            self.Damage(random.randint(enemy.minDamage, enemy.maxDamage))
+            enemy.Damage(random.randint(self.minDamage, self.maxDamage))
             game.map.ChangeChar(enemy.position, "*")
-            game.cantMoveStartTime = time.time() * 1000
+            timeNow = time.time() * 1000
+            enemy.cantMoveStartTime = timeNow
+            game.player.cantMoveStartTime = timeNow
         else:
             game.map.ChangeChar(self.position, " ")
             game.map.ChangeChar(newPosition, "+")
             self.position = newPosition
 
 class Enemy():
-    health = 5
+    health = 10
     position = Position(9, 5)
-    path = [Position(0, -1), Position(0, -1), Position(0, 1), Position(0, 1), Position(0, 1), Position(0, 1), Position(0, -1), Position(0, -1)] #the position the enemy needs to walk
+    path = [] #the position the enemy needs to walk
     pathCount = 0
-    walkDelay = 500 # is in miliseconds
+    walkDelay = 500 # is in milliseconds
     lastPathChange = 0 #ms time
     canMove = True
     Dead = False
+    minDamage = 2
+    maxDamage = 8
+    cantMoveStartTime = 0
+
+    def __init__(self, health, position, path, walkDelay, minDamage, maxDamage):
+        self.health = health
+        self.position = position
+        self.path = path
+        self.walkDelay = walkDelay
+        self.minDamage = minDamage
+        self.maxDamage = maxDamage
     
     def MovePath(self):
         newPosition = Position(self.position.x + self.path[self.pathCount].x, self.position.y + self.path[self.pathCount].y)
@@ -73,10 +120,13 @@ class Enemy():
             pass
         if(charOnNewPosition == "+"):
             #enemy on player
-            game.player.Damage(2)
+            game.player.Damage(random.randint(self.minDamage, self.maxDamage))
+            self.Damage(random.randint(game.player.minDamage, game.player.maxDamage))
             self.canMove = False
             game.player.canMove = False
-            game.cantMoveStartTime = time.time() * 1000
+            timeNow = time.time() * 1000
+            self.cantMoveStartTime = timeNow
+            game.player.cantMoveStartTime = timeNow
             game.map.ChangeChar(self.position, "*")
         else:
             game.map.ChangeChar(self.position, " ")
@@ -134,10 +184,13 @@ class Map():
 
 class Game():
     player = Player()
-    enemies = [Enemy()]
+    enemies = []
     map = Map(player, enemies)
+    Started = False
 
-    cantMoveStartTime = 0
+    def __init__(self):
+        self.enemies.append(Enemy(10, Position(9, 5), [Position(0, -1), Position(0, -1), Position(0, 1), Position(0, 1), Position(0, 1), Position(0, 1), Position(0, -1), Position(0, -1)], 500, 2, 8))
+        self.enemies.append(Enemy(10, Position(15, 2), [Position(-1, 0), Position(-1, 0), Position(-1, 0), Position(1, 0), Position(1, 0), Position(1, 0)], 500, 2, 8))
 
     def GetEnemy(self, position):
         for enemy in self.enemies:
@@ -150,6 +203,7 @@ class Game():
                 self.map.ChangeChar(self.enemies[i].position, " ")
                 self.enemies.pop(i)
                 self.UpdateScreen()
+                break
 
 
     def UpdateScreen(self):
@@ -162,32 +216,29 @@ class Game():
             char = self.currentMap[i]
             if(char == "+"):
                 char = bcolors.OKGREEN + char + bcolors.ENDC
-            elif(char == "x"):
+            elif(char == "x" or char == "*"):
                 char = bcolors.FAIL + char + bcolors.ENDC
             sys.stdout.write(char + " ")
             sys.stdout.flush()
 
-        print("\n")
+        print("\n\nHealth: " + str(game.player.health))
 
+        print("\n" + str(userInput))
 
-game = Game()
-game.UpdateScreen()
-
-UpdateScreen = False
+        print("")
 
 def MoveEnemies():
     global UpdateScreen
     timeNow = time.time() * 1000
-    for enemy in game.enemies:
+    for i in range(len(game.enemies) - 1, -1, -1):
+        enemy = game.enemies[i]
         if(enemy.canMove):
             if(timeNow > (enemy.lastPathChange + enemy.walkDelay)):
                 enemy.lastPathChange = timeNow
                 enemy.MovePath()
                 UpdateScreen = True
-    
-    if(timeNow > (game.cantMoveStartTime + EnemyAttackDelay)):
-        for enemy in game.enemies:
-            if(not enemy.canMove):
+        else:
+            if(timeNow > (enemy.cantMoveStartTime + EnemyAttackDelay)):
                 enemy.canMove = True
                 if(enemy.Dead):
                     game.map.ChangeChar(enemy.position, " ")
@@ -196,50 +247,108 @@ def MoveEnemies():
                     game.map.ChangeChar(enemy.position, "x")
                 UpdateScreen = True
 
-    if(timeNow > (game.cantMoveStartTime + AttackDelay)):
-        game.player.canMove = True
+    if(not game.player.canMove):
+        if(timeNow > (game.player.cantMoveStartTime + AttackDelay)):
+            game.player.canMove = True
+
+def RunCommand(command):
+    if(command.lower().strip() == "exit" or command.lower().strip() == "quit"):
+        os._exit(0)
 
 def on_press(key):
     global UpdateScreen
+    global MoveKeyDown
+    global userInput
     #print('{0} pressed'.format(key))
-    if(game.player.canMove):
-        if(key == Key.right):
-            game.player.Move(1, 0)
-            #callback_queue.put(lambda: game.player.Move(1, 0))
+
+    if(game.Started):
+        if(key == Key.enter):
+            RunCommand(userInput)
+            userInput = ""
             UpdateScreen = True
-        elif(key == Key.left):
-            game.player.Move(-1, 0)
-            #callback_queue.put(lambda: game.player.Move(-1, 0))
+        elif(key == Key.backspace):
+            userInput = userInput[:-1]
             UpdateScreen = True
-        elif(key == Key.up):
-            game.player.Move(0, -1)
-            #callback_queue.put(lambda: game.player.Move(0, -1))
+        elif(key == Key.space):
+            userInput += " "
             UpdateScreen = True
-        elif(key == Key.down):
-            game.player.Move(0, 1)
-            #callback_queue.put(lambda: game.player.Move(0, 1))
+        try:
+            userInput += str(key.char)
             UpdateScreen = True
+        except Exception:
+            pass
+
+        if(MoveKeyDown is None):
+            if(game.player.canMove):
+                if(key == Key.right):
+                    MoveKeyDown = key
+                    game.player.Move(1, 0)
+                    #callback_queue.put(lambda: game.player.Move(1, 0))
+                    UpdateScreen = True
+                elif(key == Key.left):
+                    MoveKeyDown = key
+                    game.player.Move(-1, 0)
+                    #callback_queue.put(lambda: game.player.Move(-1, 0))
+                    UpdateScreen = True
+                elif(key == Key.up):
+                    MoveKeyDown = key
+                    game.player.Move(0, -1)
+                    #callback_queue.put(lambda: game.player.Move(0, -1))
+                    UpdateScreen = True
+                elif(key == Key.down):
+                    MoveKeyDown = key
+                    game.player.Move(0, 1)
+                    #callback_queue.put(lambda: game.player.Move(0, 1))
+                    UpdateScreen = True
+    else:
+        if(key == Key.space):
+            game.Started = True
 
 def on_release(key):
-    pass
+    global UpdateScreen
+    global MoveKeyDown
+    if(key == MoveKeyDown):
+        MoveKeyDown = None
+        UpdateScreen = True
+
+game = Game()
+
+UpdateScreen = False
 
 listener = Listener(
     on_press=on_press,
     on_release=on_release)
 listener.start()
 
+updateDelay = 300 #in milliseconds
+lastUpdate = -2000
+onoff = False
+
+while not game.Started: 
+    timeNow = time.time() * 1000
+    if(timeNow > (lastUpdate + updateDelay)):
+        clear()
+        print (asciiArt)
+        lastUpdate = timeNow
+        if(onoff):
+            onoff = False
+        else: 
+            onoff = True
+            print("Press SPACE to play")
+
 while True:
-    """
-    try:
-        callback = callback_queue.get(False)
-        callback()
-    except queue.Empty:
-        pass
-    """
+    if(game.Started):
+        """
+        try:
+            callback = callback_queue.get(False)
+            callback()
+        except queue.Empty:
+            pass
+        """
 
-    MoveEnemies()
-    if(UpdateScreen):
-        game.UpdateScreen()
-        UpdateScreen = False
+        MoveEnemies()
+        if(UpdateScreen):
+            game.UpdateScreen()
+            UpdateScreen = False
 
-input("Press enter to continue")
+input("Press enter to exit")
